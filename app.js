b7398e42e11aa332a2118820205adcef13d47559
SuperTurban <tambet.uutsalu@gmail.com>
2017-10-21 23:32:29 +0300

    #22 port number from env variable

--- app.js
+++ app.js
@@ -53,16 +53,16 @@ app.use(function(req, res, next) {
 
 
 
-
+/*
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   res.status(err.status || 500);
   return; 
 });
+*/
 
 
 app.listen(process.env.PORT || 3000, function () {
