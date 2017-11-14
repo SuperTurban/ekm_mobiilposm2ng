29864ab6e60e26b86f1cbf04924e45f546ebf8c7
Joel Kuppart <Joel.Kuppart@mg.ee>
2017-11-14 20:50:30 +0200

    #91 UI tests for web app use cases 1 and 2

--- routes/user.js
+++ routes/user.js
@@ -10,17 +10,18 @@ let bcrypt = require('bcrypt');
 module.exports = function(app){
 
     app.get(base_path + '/maketestadminuser', function(req,res){
-        let password = bcrypt.hash('testpassword', 10)
+        bcrypt.hash('testpassword', 10)
             .then(function(hash){
-
+					
                     let user = new User({
                         username : 'ekmadmin',
                         password : hash,
                         admin : 1,
                         email : 'admin@localhost'
                     })
-
+										
                     user.save(function(err, r){
+						console.log('jou');
                         if(err)
                             return console.log(err);
                         else{
