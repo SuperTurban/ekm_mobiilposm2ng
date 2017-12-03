fa657a99f39d09169ae2bf587a815e30b688dca0
Joel Kuppart <Joel.Kuppart@mg.ee>
2017-12-03 14:01:21 +0200

    #70 Fixed tab changing UI

--- client/src/components/mainmenu.vue
+++ client/src/components/mainmenu.vue
@@ -2,14 +2,17 @@
 <div id = "main-menu">
 <nav class="navbar navbar-light">
 <ul class="nav nav-tabs nav-fill">
-  <li class="nav-item">
-    <router-link to="/games" class="nav-link active" href="#">Mängud</router-link>
+  <li class="nav-item" @click="clicked(0)">
+    <router-link to="/games" class="nav-link" v-bind:class="{ 'active' : activeEl == 0}" href="#">Mängud</router-link>
   </li>
-  <li class="nav-item">
-    <router-link  class="nav-link" to="/destinations">Mängude sihtkohad</router-link>
+  <li class="nav-item" @click="clicked(1)">
+    <router-link  class="nav-link" v-bind:class="{ 'active' : activeEl == 1}" to="/destinations">Mängude sihtkohad</router-link>
   </li>
-  <li class="nav-item">
-    <router-link to="/users" class="nav-link" href="#">Kasutajad</router-link>
+  <li class="nav-item" @click="clicked(2)">
+    <router-link to="/users" class="nav-link" v-bind:class="{ 'active' : activeEl == 2}" href="#">Kasutajad</router-link>
+  </li>
+    <li class="nav-item" @click="clicked(3)">
+    <router-link to="/leaderboards" class="nav-link" v-bind:class="{ 'active' : activeEl == 3}" href="#">Edetabelid</router-link>
   </li>
 </ul>
 <ul class="nav nav-pills justify-content-end">
@@ -26,16 +29,20 @@ export default {
   name: 'mainmenu',  
   data () {
     return {
-      
+	  activeEl : 0
     }
   },
   methods : {
-    logout : function(){
+    logout : function(event){
       this.$parent.state.loggedIn = false;
       this.api.token = false;
-    }
+    },
+	clicked : function(el) {
+	  this.activeEl = el;
+	}
   }
 }
+
 </script>
 <style>
 #main-menu{
